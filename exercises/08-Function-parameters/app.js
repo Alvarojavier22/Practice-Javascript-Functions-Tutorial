0c7387beae2462f0dd8e649dffd49ca13da8f5e7
Alvaro Javier <alvarojavierchagas@hotmail.com>
2022-11-02 22:37:26 +0000

    Casi teerminado

--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,6 +1,8 @@
 // Your code goes here:
+let renderPerson = function () {
+  return "Bob " + "is a " + 23 + " years old male " + "born in 05/22/1983 " + "with green yes. "
+}
 
 
-
-  ///*** Do not edit below this line ****/
-  console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+///*** Do not edit below this line ****/
+console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
